0c04d3587eadd10b2524a25304f01d0aa9be3274
Sahil Umraniya <sahilumraniya9512@gmail.com>
2024-11-25 16:02:17 +0530

    fix room delete

--- src/services/v1/room-management/room-management.hooks.ts
+++ src/services/v1/room-management/room-management.hooks.ts
@@ -1,14 +1,16 @@
 import { HooksObject } from '@feathersjs/feathers';
 import * as authentication from '@feathersjs/authentication';
+import SetDefaultQuery from '../../../hooks/SetDefaultQuery';
+import { RoomStatus } from '../../../db_services/v1/room/interfaces/RoomInterface';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
 
 export default {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
+    all: [authenticate('jwt')],
+    find: [SetDefaultQuery('status', RoomStatus.ACTIVE)],
+    get: [SetDefaultQuery('status', RoomStatus.ACTIVE)],
     create: [],
     update: [],
     patch: [],
